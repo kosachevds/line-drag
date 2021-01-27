f7d0b3723b5ae649d8c69ec01d2ba93258ed514b
Kosachev Daniil <kos-dan@mail.ru>
2021-01-27 15:36:43 +0300

    Remade with ECG units

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,26 @@ import Canvas from './Canvas';
 import './styles.css';
 import recordPage from "./images/recordPage.png"
 
-const MM_PER_PX = 1.0 / 196.0;
-const CM_UNITS = "cm"
+const MM_PER_PX = 1.0 / 1.96;
+const MM_PER_MV = 10;
+const MM_PER_SEC = 25;
+const MV_PER_PX = MM_PER_PX / MM_PER_MV;
+const SEC_PER_PX = MM_PER_PX / MM_PER_SEC;
+enum Units {
+    Centimeters = "cm",
+    Millivolts = "mV",
+    Seconds = "sec",
+}
+
 
 function App() {
     return (
         <div className="App" style={{backgroundImage: `url(${recordPage})`}}>
             <Canvas
-                widthCoefficient={MM_PER_PX * 10}
-                heightCoefficient={MM_PER_PX * 10}
-                widthUnit={CM_UNITS}
-                heightUnit={CM_UNITS}
+                widthCoefficient={SEC_PER_PX}
+                heightCoefficient={MV_PER_PX}
+                widthUnit={Units.Seconds}
+                heightUnit={Units.Millivolts}
             />
         </div>
     );
