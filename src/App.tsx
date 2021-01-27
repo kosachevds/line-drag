2886a9dacdf882019b5a8d88e72e948cabcfad75
Kosachev Daniil <kos-dan@mail.ru>
2021-01-27 14:52:22 +0300

    Passed props for cantimiters

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,18 @@ import Canvas from './Canvas';
 import './styles.css';
 import recordPage from "./images/recordPage.png"
 
+const CM_PER_PX = 1 / 19.6;
+const CM_UNITS = "cm"
+
 function App() {
     return (
         <div className="App" style={{backgroundImage: `url(${recordPage})`}}>
-            <Canvas />
+            <Canvas
+                widthCoefficient={CM_PER_PX}
+                heightCoefficient={CM_PER_PX}
+                widthUnit={CM_UNITS}
+                heightUnit={CM_UNITS}
+            />
         </div>
     );
 }
