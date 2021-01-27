dc715936eb41e18f89af1a99f33adbecfe6e2262
Kosachev Daniil <kos-dan@mail.ru>
2021-01-27 14:58:18 +0300

    Remade coefficients

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,15 @@ import Canvas from './Canvas';
 import './styles.css';
 import recordPage from "./images/recordPage.png"
 
-const CM_PER_PX = 1 / 19.6;
+const MM_PER_PX = 1.0 / 196.0;
 const CM_UNITS = "cm"
 
 function App() {
     return (
         <div className="App" style={{backgroundImage: `url(${recordPage})`}}>
             <Canvas
-                widthCoefficient={CM_PER_PX}
-                heightCoefficient={CM_PER_PX}
+                widthCoefficient={MM_PER_PX * 10}
+                heightCoefficient={MM_PER_PX * 10}
                 widthUnit={CM_UNITS}
                 heightUnit={CM_UNITS}
             />
