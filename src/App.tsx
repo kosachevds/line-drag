35a8f8e4335a2541ed891eab68786122e334ab8c
Kosachev Daniil <kos-dan@mail.ru>
2021-01-27 14:45:44 +0300

    Added image to background

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Canvas from './Canvas';
 import './styles.css';
+import recordPage from "./images/recordPage.png"
 
 function App() {
     return (
-        <div className="App">
+        <div className="App" style={{backgroundImage: `url(${recordPage})`}}>
             <Canvas />
         </div>
     );
